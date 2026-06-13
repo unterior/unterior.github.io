4a5a33bf425f1a4b9e64f85a393441bacf1ce0e7
unterior <hshs9437@gmail.com>
2026-06-13 14:32:29 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function scrollToSection(id) {
+  const section = document.getElementById(id);
+
+  if (section) {
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  }
+}
